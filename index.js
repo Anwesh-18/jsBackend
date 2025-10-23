564284de062771add59d7ca7316ff73f0b42e1dc
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-10-23 21:59:21 +0530

    learning express

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-require('dotenv').config()
-const express = require('express')
+// import { configDotenv } from 'dotenv';
+// require('dotenv').config();
+// const express = require('express')
+import express from 'express'; // this will give error unless we add type:'module' in package.json
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
-const port = 4000
+const port = process.env.PORT||4000
 
 app.get('/',(req,res) => {
     res.send('Hello world!')
 })
 
-app.get('/twitter',(req,res)=>{
-    res.send(`Anwesh_`)
-})
-
-app.get('/login',(req,res)=>{
-    res.send(`<h2>Please login here!</h2>`)
-})
+app.get('/jokes',(req,res)=>{
+    const jokes = [
+        {
+            id: "joke1",
+            title: "first joke"
+        },
+        {
+            id: "joke2",
+            title: "first joke"
+        },
+        {
+            id: "joke3",
+            title: "third joke"
+        },
+        {
+            id: "joke4",
+            title: "fourth joke"
+        },
+        {
+            id: "joke5",
+            title: "fifth joke"
+        }
+    ]
+    res.send(jokes);
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Example app listening on port ${port}`);
