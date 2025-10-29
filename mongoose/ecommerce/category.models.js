449e46a6fa232e21b30af45b2d47f105dccaec0d
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-10-29 18:57:21 +0530

    data handling

---
+++ mongoose/ecommerce/category.models.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const categorySchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    }
+},{timestamps:true});
+
+const category = mongoose.model("category",categorySchema)
